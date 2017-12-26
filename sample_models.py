d494d8f3806b9a73fc45987146a1c578dc105ed8
André Gomes Lamas Otero <lamas3000@gmail.com>
2017-12-26 15:20:01 -0200

    feat: Model 1
    Add Model 1 with recurrent layer and BatchNormalization layer

--- sample_models.py
+++ sample_models.py
@@ -19,24 +19,30 @@ def simple_rnn_model(input_dim, output_dim=29):
     print(model.summary())
     return model
 
+
 def rnn_model(input_dim, units, activation, output_dim=29):
     """ Build a recurrent network for speech 
     """
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
+
     # Add recurrent layer
     simp_rnn = GRU(units, activation=activation,
-        return_sequences=True, implementation=2, name='rnn')(input_data)
-    # TODO: Add batch normalization 
-    bn_rnn = ...
-    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+                   return_sequences=True, implementation=2, name='rnn')(input_data)
+
+    # Add batch normalization
+    bn_rnn = BatchNormalization()(simp_rnn)
+
+    # Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
+
     # Specify the model
     model = Model(inputs=input_data, outputs=y_pred)
     model.output_length = lambda x: x
-    print(model.summary())
+    model.summary()
     return model
 
 
