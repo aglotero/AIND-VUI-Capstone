e82ccb6dfcf8e4e0321258586f881f52f7f1ea28
André Gomes Lamas Otero <lamas3000@gmail.com>
2017-12-26 15:22:53 -0200

    feat: Model 2
    Add Model 2 with Recurrent, BatchNormalization and TimeDistributed layes

--- sample_models.py
+++ sample_models.py
@@ -47,33 +47,39 @@ def rnn_model(input_dim, units, activation, output_dim=29):
 
 
 def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
-    conv_border_mode, units, output_dim=29):
+                  conv_border_mode, units, output_dim=29):
     """ Build a recurrent + convolutional network for speech 
     """
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # Add convolutional layer
-    conv_1d = Conv1D(filters, kernel_size, 
-                     strides=conv_stride, 
+    conv_1d = Conv1D(filters, kernel_size,
+                     strides=conv_stride,
                      padding=conv_border_mode,
                      activation='relu',
                      name='conv1d')(input_data)
+
     # Add batch normalization
     bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+
     # Add a recurrent layer
     simp_rnn = SimpleRNN(units, activation='relu',
-        return_sequences=True, implementation=2, name='rnn')(bn_cnn)
-    # TODO: Add batch normalization
-    bn_rnn = ...
-    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+                         return_sequences=True, implementation=2, name='rnn')(bn_cnn)
+
+    # Add batch normalization
+    bn_rnn = BatchNormalization()(simp_rnn)
+
+    # Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
+
     # Specify the model
     model = Model(inputs=input_data, outputs=y_pred)
     model.output_length = lambda x: cnn_output_length(
         x, kernel_size, conv_border_mode, conv_stride)
-    print(model.summary())
+    model.summary()
     return model
 
 def cnn_output_length(input_length, filter_size, border_mode, stride,
